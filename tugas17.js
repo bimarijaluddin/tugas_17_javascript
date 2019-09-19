ebc0e7489ca6c3b9a920e56874e8b0cf08acf38c
bima rijaluddin <bimarijal@gmail.com>
2019-09-19 20:28:31 +0800

    Tugas 17 Javascript
    tugas dari niomic

---
+++ tugas17.js
@@ -0,0 +1,14 @@
+function tugas17(){
+    var kata1 = "Saya beLajar bahaSa peMrograman deNgan khUsuk";
+    var kata2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual";
+
+    console.log(kata1.toUpperCase());
+    console.log(kata1.toLowerCase());
+
+    console.log(kata2.toUpperCase());
+    console.log(kata2.toLowerCase());
+}
+
+tugas17();
+
+
